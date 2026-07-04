9cf560b0b035dce08a8251afb8487409f10f7fd6
Erruar <erruarbrorder@gmail.com>
2026-07-04 20:55:50 +0300

    Started migrating Client services to Core and Service Use previous commit to build app

---
+++ Saku Overclock.Core/Contracts/IDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using Saku_Overclock.Shared.Models;
+
+namespace Saku_Overclock.Core.Contracts;
+
+public interface IDataProvider
+{
+    /// <summary>
+    ///     Получает и обновляет данные сенсоров для мониторинга.
+    /// </summary>
+    void GetData(ref SensorsInformation sensorsInformation);
+
+    /// <summary>
+    ///     Получить таблицу сенсоров устройства
+    /// </summary>
+    /// <returns>Таблица сенсоров устройства</returns>
+    float[]? GetPowerTable();
+}
---
+++ Saku Overclock.Core/Contracts/ISensorIndexResolver.cs
@@ -0,0 +1,14 @@
+﻿using Saku_Overclock.Shared.Models;
+
+namespace Saku_Overclock.Core.Contracts;
+
+public interface ISensorIndexResolver
+{
+    /// <summary>
+    ///     Получить индекс в таблице сенсоров устройства для требуемого индекса
+    /// </summary>
+    /// <param name="tableVersion">Версия таблицы сенсоров устройства (получить от Smu)</param>
+    /// <param name="sensor">Тип требуемого сенсора</param>
+    /// <returns>Индекс в таблице сенсоров устройства</returns>
+    int ResolveIndex(int tableVersion, SensorId sensor);
+}
